29f58eeb69a625333737a5fccc9be9fbe7e0f4d6
panghwi <kanean99@naver.com>
2022-12-22 21:03:23 +0900

    first commit

--- toyproject/src/App.js
+++ toyproject/src/App.js
@@ -3,17 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          토이프로젝트 10조
-        </p>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>토이프로젝트 10조 안광휘 수정</p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
