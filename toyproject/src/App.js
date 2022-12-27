b9674897851c2c0cd2b16d8b4d5c593b608be85b
Chaenniee <susan2034@snu.ac.kr>
2022-12-27 22:27:33 +0900

    feat / App.js에 로그인 컴포넌트 삽입

--- toyproject/src/App.js
+++ toyproject/src/App.js
@@ -1,23 +1,8 @@
-import logo from "./logo.svg";
+import Loginpage from "./components/Loginpage";
 import "./App.css";
 
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>토이프로젝트 10조 안광휘 수정</p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Loginpage></Loginpage>;
 }
 
 export default App;
