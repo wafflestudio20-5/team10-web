78c8b912d0b80537b9fb5cdd96ba88694c03d3ff
2019dahn <>
2022-12-28 18:50:08 +0900

    feat / 회원가입 입력창, 회원가입 완료 화면 구현

--- toyproject/src/SignUp.tsx
+++ toyproject/src/SignUp.tsx
@@ -1,13 +1,13 @@
-import {useState} from "react";
+import React, {useEffect, useState} from "react";
 import ReactMarkdown from "react-markdown";
+import {ToastContainer, toast} from "react-toastify";
 import "./SignUp.scss";
-import {Term1, Term2, Term3} from "./TermsOfService.ts";
+import {Term1, Term2, Term3} from "./TermsOfService.js";
 
 
 /** 이용 약관 */
 interface Term {
     title: string,
-    agreed: boolean,
     content: JSX.Element
 }
 
@@ -16,57 +16,195 @@ const studentIdRegex: RegExp = /\d{4}-\d{5}/;
 
 /** 회원 가입하는 유저 정보 양식 */
 interface UserInfo {
+    type: string,
     username: string,
     password: string,
     id: string,
 }
 
-export default function SignUp() {
-    const [termsOfService, setTermsOfService] = useState<Term[]>([
+const TermsElement = ({
+                          currentStage,
+                          setCurrentStage
+                      }: { currentStage: number, setCurrentStage: React.Dispatch<number> }) => {
+    const termsOfService: Term[] = [
         {
             title: "서울대학교 계정 관리 지침",
-            agreed: false,
             content: <ReactMarkdown className="tos-content">{Term1}</ReactMarkdown>,
         },
         {
             title: "서울대학교 포털 운영 지침",
-            agreed: false,
             content: <ReactMarkdown className="tos-content">{Term2}</ReactMarkdown>
         },
         {
             title: "개인정보 수집∙이용 및 제공에 대한 안내",
-            agreed: false,
             content: <ReactMarkdown className="tos-content">{Term3}</ReactMarkdown>
         }
-    ]);
-    const [currentStage, setCurrentStage] = useState<number>(0);
-    const [userInfo, setUserInfo] = useState<UserInfo>({
-        username: "",
-        password: "",
-        id: "",
+    ];
+    const [checkedAll, setCheckedAll] = useState<boolean>(false);
+    const [checkedArr, setCheckedArr] = useState<boolean[]>([false, false, false]);
+
+    useEffect(() => {
+        setCheckedAll(checkedArr[0] && checkedArr[1] && checkedArr[2]);
     });
 
     return (
-        <div className="signup-wrapper">
+        <div>
             <div className="tos-header all">
-                <input type="checkbox" />
+                <input type="checkbox" checked={checkedAll} onChange={() => {
+                    checkedAll ? setCheckedArr([false, false, false]) : setCheckedArr([true, true, true]);
+                }}/>
                 <p className="tos-header-title all">서울대학교 운영지침과 개인정보 수집 및 이용에 모두 동의합니다.</p>
             </div>
             <div className="tos-header">
-                <input type="checkbox"/>
+                <input type="checkbox" checked={checkedArr[0]}
+                       onChange={() => setCheckedArr([!checkedArr[0], checkedArr[1], checkedArr[2]])}/>
                 <p className="tos-header-title">{termsOfService[0].title}</p>
             </div>
             {termsOfService[0].content}
             <div className="tos-header">
-                <input type="checkbox"/>
+                <input type="checkbox" checked={checkedArr[1]}
+                       onChange={() => setCheckedArr([checkedArr[0], !checkedArr[1], checkedArr[2]])}/>
                 <p className="tos-header-title">{termsOfService[1].title}</p>
             </div>
             {termsOfService[1].content}
             <div className="tos-header">
-                <input type="checkbox"/>
+                <input type="checkbox" checked={checkedArr[2]}
+                       onChange={() => setCheckedArr([checkedArr[0], checkedArr[1], !checkedArr[2]])}/>
                 <p className="tos-header-title">{termsOfService[2].title}</p>
             </div>
             {termsOfService[2].content}
+            <button className="signup-next" onClick={() => {
+                checkedAll ? setCurrentStage(currentStage + 1) : toast("동의를 선택하면 다음 단계로 이동합니다.", {
+                    position: "top-center",
+                    theme: "colored"
+                });
+            }}>다음
+            </button>
+        </div>
+    )
+}
+
+const ProgressElement = ({currentStage}: { currentStage: number }) => {
+    return (
+        <ol className="progress">
+            <li className={currentStage === 0 ? "progress-step active" : "progress-step"}>
+                <div className="image-crop">
+                    <img src={"https://nsso.snu.ac.kr/sso/resources/snu/usr/images/img_step01.png"} alt="운영방침 동의"
+                         className="progress-logo"/>
+                </div>
+                운영방침 동의
+            </li>
+            <img src={"https://nsso.snu.ac.kr/sso/resources/snu/usr/images/step_arrow.png"} alt="arrow"
+                 className="progress-arrow"/>
+            <li className={currentStage === 1 ? "progress-step active" : "progress-step"}>
+                <div className="image-crop">
+                    <img src={"https://nsso.snu.ac.kr/sso/resources/snu/usr/images/img_step04.png"} alt="운영방침 동의"
+                         className="progress-logo"/>
+                </div>
+                정보입력
+            </li>
+            <img src={"https://nsso.snu.ac.kr/sso/resources/snu/usr/images/step_arrow.png"} alt="arrow"
+                 className="progress-arrow"/>
+            <li className={currentStage === 2 ? "progress-step active" : "progress-step"}>
+                <div className="image-crop">
+                    <img src={"https://nsso.snu.ac.kr/sso/resources/snu/usr/images/img_step05.png"} alt="운영방침 동의"
+                         className="progress-logo"/>
+                </div>
+                완료
+            </li>
+        </ol>
+    )
+}
+
+export default function SignUp() {
+
+    /** 현재 회원가입 진행 상황 */
+    const [currentStage, setCurrentStage] = useState<number>(0);
+    const [userInfo, setUserInfo] = useState<UserInfo>({
+        type: "student",
+        username: "",
+        password: "",
+        id: "",
+    });
+    const [pwChecking, setPwChecking] = useState<boolean>(false);
+    const [pwRepeat, setPwRepeat] = useState<string>("");
+
+    return (
+        <div className="signup-wrapper">
+            <ProgressElement currentStage={currentStage}/>
+            {
+                currentStage === 0 ?
+                    <TermsElement currentStage={currentStage} setCurrentStage={setCurrentStage}/> :
+                    currentStage === 1 ?
+                        <div className="signup-wrapper">
+                            <h2>개인정보 입력</h2>
+                            <div className="signup-notice">※ 인적정보가 등록된 구성원만 계정 신청을 할 수 있습니다.</div>
+                            <ul className="usertype-wrapper">
+                                <li className="usertype" onClick={() => setUserInfo({...userInfo, type: "student"})}>
+                                    <div className="radio"><div className={userInfo.type === "student" ? "radio inner on" : "radio inner"}></div></div>
+                                    <img src={userInfo.type === "student"
+                                        ? "https://nsso.snu.ac.kr/sso/resources/snu/usr/images/student_on.svg"
+                                        : "https://nsso.snu.ac.kr/sso/resources/snu/usr/images/student.svg"}
+                                         alt="student" className="usertype-image"/>
+                                    <h4>학생 ∙ 졸업생</h4>
+                                </li>
+                                <li className="usertype" onClick={() => setUserInfo({...userInfo, type: "professor"})}>
+                                    <div className="radio"><div className={userInfo.type === "professor" ? "radio inner on" : "radio inner"}></div></div>
+                                    <img src={userInfo.type === "professor"
+                                        ? "https://nsso.snu.ac.kr/sso/resources/snu/usr/images/teacher_on.svg"
+                                        : "https://nsso.snu.ac.kr/sso/resources/snu/usr/images/teacher.svg"}
+                                         alt="professor" className="usertype-image"/>
+                                    <h4>교직원</h4>
+                                </li>
+                            </ul>
+                            <div className="userinfo-wrapper">
+                                <div className="input-wrapper">
+                                    <p className="input-title">이름</p>
+                                    <input type="text" placeholder="홍길동" className="input"/>
+                                </div>
+                                <div className="input-wrapper">
+                                    <p className="input-title">아이디</p>
+                                    <input type="text" className="input" onChange={
+                                        event => setUserInfo({...userInfo, username: event.target.value})
+                                    }/>
+                                </div>
+                                <div className="input-wrapper">
+                                    <p className="input-title">비밀번호</p>
+                                    <input type="password" className="input" onChange={
+                                        event => setUserInfo({...userInfo, password: event.target.value})
+                                    }/>
+                                </div>
+                                <div className="input-wrapper">
+                                    <p className="input-title">비밀번호 확인</p>
+                                    <div className="password-repeat">
+                                        <input type="password" className="input" onChange={
+                                            event => {
+                                                setPwChecking(true);
+                                                setPwRepeat(event.target.value)
+                                            }
+                                        }/>
+                                        {
+                                            pwChecking ?
+                                            <img className="password-alert" src={
+                                                pwRepeat === userInfo.password
+                                                    ? "https://cdn-icons-png.flaticon.com/512/5610/5610944.png"
+                                                    : "https://cdn-icons-png.flaticon.com/512/1828/1828843.png"
+                                            } alt="비밀번호 확인"/> : null
+                                        }
+                                    </div>
+                                </div>
+                            </div>
+                            <button className="signup-next" onClick={
+                                () => setCurrentStage(currentStage+1)
+                            }>다음</button>
+                        </div>
+                        :
+                        <div className="signup-wrapper">
+                            <h2 className="signup-done">회원가입이 완료되었습니다.</h2>
+                            <button className="signup-next">돌아가기</button>
+                        </div>
+            }
+            <ToastContainer/>
         </div>
     );
 }
