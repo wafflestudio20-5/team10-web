3ff047c5957e79cce2408db0bf8486cc2fa41065
panghwi <kanean99@naver.com>
2022-12-29 15:50:28 +0900

    feat / 라우팅 임시로 추가

--- toyproject/src/App.tsx
+++ toyproject/src/App.tsx
@@ -3,16 +3,46 @@ import './App.css';
 import { SideNavBar } from './components/sideNavbar/SideNavBar';
 import { DashBoard } from './components/dashboard/DashBoard';
 import { RightSide } from './components/dashboard/RightSide';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SignUp from './components/SignUp';
+import Loginpage from './components/LoginPage';
 
 function App() {
   return (
-    <div className='wrapper'>
-      <SideNavBar />
-      <div className='body'>
-        <DashBoard />
-        <RightSide />
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <>
+              <div className='wrapper'>
+                <SideNavBar />
+                <div className='body'>
+                  <DashBoard />
+                  <RightSide />
+                </div>
+              </div>
+            </>
+          }
+        ></Route>
+        <Route
+          path='/login/new'
+          element={
+            <>
+              <SignUp></SignUp>
+            </>
+          }
+        ></Route>
+        <Route
+          path='/login'
+          element={
+            <>
+              <Loginpage></Loginpage>
+            </>
+          }
+        ></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
