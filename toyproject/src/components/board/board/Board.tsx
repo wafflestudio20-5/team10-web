b43075d7abf54a9da8e1f79e48eec32779f12a76
panghwi <kanean99@naver.com>
2022-12-29 17:06:53 +0900

    feat / boardDetailPage부터 만들기 시작

--- toyproject/src/components/board/board/Board.tsx
+++ toyproject/src/components/board/board/Board.tsx
@@ -2,5 +2,9 @@ import React from 'react';
 import styles from './Board.module.scss';
 
 export default function Board() {
-  return <div className={styles.wrapper}>네</div>;
+  return (
+    <div className={styles.wrapper}>
+      <h2>전체 게시판</h2>
+    </div>
+  );
 }
