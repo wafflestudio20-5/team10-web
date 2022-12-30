67a634f64f85d5a4575c0c64cd16f4862a5ff7bb
panghwi <kanean99@naver.com>
2022-12-30 17:15:49 +0900

    feat / fontawesome, react markdown 사용한 것 반영

--- toyproject/src/components/board/boardNav/BoardNav.tsx
+++ toyproject/src/components/board/boardNav/BoardNav.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import styles from './BoardNav.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClipboardQuestion } from '@fortawesome/free-solid-svg-icons';
+import {
+  faClipboardList,
+  faClipboardQuestion,
+} from '@fortawesome/free-solid-svg-icons';
 export default function BoardNav() {
   return (
     <div className={styles.wrapper}>
       <h2>전체 게시판</h2>
       <nav>
         <ul>
-          <FontAwesomeIcon icon={faClipboardQuestion} />
-          <li>공지 게시판</li>
-          <li>qna 게시판</li>
+          <li>
+            <FontAwesomeIcon icon={faClipboardList} size='lg'></FontAwesomeIcon>
+            공지 게시판
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faClipboardQuestion} size='lg' />
+            qna 게시판
+          </li>
         </ul>
       </nav>
     </div>
