e75c15b2b3a5a8d55c49414666fb8817ea918d13
panghwi <kanean99@naver.com>
2022-12-31 20:45:24 +0900

    feat / 게시판 디테일 화면 css 구현

--- toyproject/src/components/board/boardDetail/BoardDetail.tsx
+++ toyproject/src/components/board/boardDetail/BoardDetail.tsx
@@ -1,18 +1,74 @@
 import React from 'react';
 import styles from './BoardDetail.module.scss';
+
+type Writing = {
+  id: number;
+  title: string;
+  username: string;
+  created_at: string; //임시
+  viewed: number; //구현할 건지 백엔드와 의논
+};
+
+const InitialWritings: Writing[] = [
+  {
+    id: 1,
+    title: '첫번째',
+    username: '광휘',
+    created_at: '2023-01-01',
+    viewed: 1,
+  },
+  {
+    id: 2,
+    title: '새해복',
+    username: '광휘',
+    created_at: '2023-01-01',
+    viewed: 1,
+  },
+  {
+    id: 3,
+    title: '많이받으세요',
+    username: '광휘',
+    created_at: '2023-01-01',
+    viewed: 1,
+  },
+];
+
 export default function BoardDetail() {
   return (
     <div className={styles.wrapper}>
-      <h2>백엔드에서 과목 정보를 받아옵니다</h2>
-      <div> ~ 게시판입니다. 공지 및 각종 질문을 올리는 곳입니다.</div>
-      <div>검색결과</div>
+      <header>
+        <h2>과목명-과목 게시판</h2>
+        <button className={styles['create-button']}>글쓰기</button>
+      </header>
+      <div className={styles.explain}>
+        과목명의 게시판입니다. 공지 및 각종 질문을 올리는 곳입니다.
+      </div>
+      <div className={styles['search-container']}>
+        검색결과 - number 개<input placeholder='검색어입력'></input>
+      </div>
       <body>
-        <header>no, 제목, 작성자, 등록일시, 조회수</header>
+        <div className={styles.category}>
+          <span className={styles.no}>no</span>
+          <span className={styles.title}>제목</span>
+          <span className={styles.username}>작성자</span>
+          <span className={styles.created_at}>등록일시</span>
+          <span className={styles.viewed}>1</span>
+        </div>
         <ul>
-          <li>여기에 컴포넌트를 return해야 할 것같음</li>
+          {InitialWritings.map((item) => {
+            return (
+              <li>
+                <span className={styles.no}>{item.id}</span>
+                <span className={styles.title}>{item.title}</span>
+                <span className={styles.username}>{item.username}</span>
+                <span className={styles.created_at}>{item.created_at}</span>
+                <span className={styles.viewed}>{item.viewed}</span>
+              </li>
+            );
+          })}
         </ul>
       </body>
-      <div>여기에 bottom nav button</div>
+      <footer>여기에 bottom nav button 이부분 회의</footer>
     </div>
   );
 }
