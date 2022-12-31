76c5db54809ac9e790b3dbe70628e9954bac1914
panghwi <kanean99@naver.com>
2022-12-31 15:48:25 +0900

    feat / 게시판 nav부분 구현중

--- toyproject/src/components/board/boardHeader/BoardHeader.tsx
+++ toyproject/src/components/board/boardHeader/BoardHeader.tsx
@@ -2,5 +2,9 @@ import React from 'react';
 import styles from './BoardHeader.module.scss';
 
 export default function BoardHeader() {
-  return <div className={styles.wrapper}>헤더</div>;
+  return (
+    <div className={styles.wrapper}>
+      <h1>과목명</h1>
+    </div>
+  );
 }
--- toyproject/src/components/board/boardSideBar/BoardSideBar.tsx
+++ toyproject/src/components/board/boardSideBar/BoardSideBar.tsx
@@ -2,5 +2,17 @@ import React from 'react';
 import styles from './BoardSideBar.module.scss';
 
 export default function BoardSideBar() {
-  return <div className={styles.wrapper}>사이드바</div>;
+  return (
+    <div className={styles.wrapper}>
+      <ul>
+        <li>홈</li>
+        <li>게시판</li>
+        <li>강의계획서</li>
+        <li>수강생</li>
+        <li>모듈</li>
+        <li>과제</li>
+        <li>성적</li>
+      </ul>
+    </div>
+  );
 }
