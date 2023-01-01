f7552951a150de42997452359f38711f557fcfe9
Chaenniee <susan2034@snu.ac.kr>
2023-01-01 22:16:28 +0900

    feat / ModulePage 컴포넌트 생성 + feat / SubjectTemplate.tsx에서 children 선택처리

--- toyproject/src/components/assignments/AssignmentPage.tsx
+++ toyproject/src/components/assignments/AssignmentPage.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
 import AssignmentBlock from "./AssignmentBlock";
 import styles from "./AssignmentPage.module.scss";
 import SubjectTemplate from "../SubjectTemplate";
 
 export default function AssignmentPage() {
-    return (
-        <SubjectTemplate subject="컴퓨터구조" page="과제" content="과제3">
-
-        </SubjectTemplate>
-    )
+  return (
+    <SubjectTemplate
+      subject='컴퓨터구조'
+      page='과제'
+      content='과제3'
+    ></SubjectTemplate>
+  );
 }
