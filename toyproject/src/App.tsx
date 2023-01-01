85310039e0fb7828f66cc3f645b2eb35752c0dcd
Chaenniee <susan2034@snu.ac.kr>
2023-01-01 21:14:21 +0900

    style / App.tsx 라우트 문법 간결화

--- toyproject/src/App.tsx
+++ toyproject/src/App.tsx
@@ -1,92 +1,32 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import SignUpPage from './components/auth/SignUpPage';
-import LoginPage from './components/auth/LoginPage';
-import BoardNavPage from './components/board/pages/BoardNavPage';
-import AssignmentPage from './components/assignments/AssignmentPage';
-import AssignmentDetailsPage from './components/assignments/AssignmentDetailsPage';
-import DashBoardPage from './components/dashboard/DashBoardPage';
-import QnABoardPage from './components/board/pages/QnABoardPage';
-import NoticeBoardPage from './components/board/pages/NoticeBoardPage';
-import NewBoardPage from './components/board/pages/NewBoardPage';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUpPage from "./components/auth/SignUpPage";
+import LoginPage from "./components/auth/LoginPage";
+import BoardNavPage from "./components/board/pages/BoardNavPage";
+import AssignmentPage from "./components/assignments/AssignmentPage";
+import AssignmentDetailsPage from "./components/assignments/AssignmentDetailsPage";
+import DashBoardPage from "./components/dashboard/DashBoardPage";
+import QnABoardPage from "./components/board/pages/QnABoardPage";
+import NoticeBoardPage from "./components/board/pages/NoticeBoardPage";
+import NewBoardPage from "./components/board/pages/NewBoardPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path='/'
-          element={
-            <>
-              <DashBoardPage></DashBoardPage>
-            </>
-          }
-        ></Route>
-        <Route
-          path='/login/new'
-          element={
-            <>
-              <SignUpPage></SignUpPage>
-            </>
-          }
-        ></Route>
-        <Route
-          path='/login'
-          element={
-            <>
-              <LoginPage></LoginPage>
-            </>
-          }
-        ></Route>
-        <Route
-          path='/:subjectname/boardnav'
-          element={
-            <>
-              <BoardNavPage></BoardNavPage>
-            </>
-          }
-        ></Route>
-        <Route
-          path='/:subjectname/qnaboard'
-          element={
-            <>
-              <QnABoardPage />
-            </>
-          }
-        ></Route>
-        <Route
-          path='/:subjectname/noticeboard'
-          element={
-            <>
-              <NoticeBoardPage></NoticeBoardPage>
-            </>
-          }
-        ></Route>
-        <Route
-          path='/:subjectname/board/new'
-          element={
-            <>
-              <NewBoardPage></NewBoardPage>
-            </>
-          }
-        ></Route>
-        <Route
-          path='/:subjectname/assignments'
-          element={
-            <>
-              <AssignmentPage></AssignmentPage>
-            </>
-          }
-        ></Route>
+        <Route path='/' element={<DashBoardPage />} />
+        <Route path='/login/new' element={<SignUpPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/:subjectname/boardnav' element={<BoardNavPage />} />
+        <Route path='/:subjectname/qnaboard' element={<QnABoardPage />} />
+        <Route path='/:subjectname/noticeboard' element={<NoticeBoardPage />} />
+        <Route path='/:subjectname/board/new' element={<NewBoardPage />} />
+        <Route path='/:subjectname/assignments' element={<AssignmentPage />} />
         <Route
           path='/:subjectname/assignments/:assignmentID'
-          element={
-            <>
-              <AssignmentDetailsPage></AssignmentDetailsPage>
-            </>
-          }
-        ></Route>
+          element={<AssignmentDetailsPage />}
+        />
       </Routes>
     </BrowserRouter>
   );
