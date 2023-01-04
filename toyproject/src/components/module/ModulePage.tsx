0d872cc9a76658d8afadbb5dcab72b824f0d940b
Chaenniee <susan2034@snu.ac.kr>
2023-01-04 14:14:46 +0900

    fix / 모듈 페이지 상단 헤더 과목명 동적으로 수정

--- toyproject/src/components/module/ModulePage.tsx
+++ toyproject/src/components/module/ModulePage.tsx
@@ -1,10 +1,12 @@
 import SubjectTemplate from "../SubjectTemplate";
 import styles from "./ModulePage.module.scss";
 import ModuleBlock from "./ModuleBlock";
+import { useParams } from "react-router-dom";
 
 export default function ModulePage() {
+  const { subjectname } = useParams();
   return (
-    <SubjectTemplate subject='컴퓨터구조' page='모듈'>
+    <SubjectTemplate subject={subjectname as string} page='모듈'>
       <ModuleBlock />
     </SubjectTemplate>
   );
