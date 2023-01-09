154979c8f5448171df4099f7718ce3e238ce3c25
panghwi <kanean99@naver.com>
2023-01-09 13:05:43 +0900

    feat / react-spinner 설치, (로딩창 라이브러리)

--- toyproject/src/components/auth/KakaoLoginPage.tsx
+++ toyproject/src/components/auth/KakaoLoginPage.tsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import ClipLoader from 'react-spinners/ClipLoader';
+import styles from './KakaoLoginPage.module.scss';
 
 export default function KakaoLoginPage() {
-  return <div></div>;
+  let code = new URL(window.location.href).searchParams.get('code');
+  console.log(code);
+
+  useEffect(() => {
+    //login
+    //set user state
+    //dashboard로 redirect
+  });
+
+  return (
+    <div className={styles.wrapper}>
+      <ClipLoader loading={true} color={'#0F0F6E'} size={60}></ClipLoader>
+    </div>
+  );
 }
