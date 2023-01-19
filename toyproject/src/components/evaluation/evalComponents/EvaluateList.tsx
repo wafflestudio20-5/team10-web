7a1902aa03c8ff400b4324aa5bbb634c311f4e76
panghwi <kanean99@naver.com>
2023-01-19 14:43:23 +0900

    feat / 강의 평가 페이지 Css, routing 연결

--- toyproject/src/components/evaluation/evalComponents/EvaluateList.tsx
+++ toyproject/src/components/evaluation/evalComponents/EvaluateList.tsx
@@ -2,19 +2,30 @@ import React from 'react';
 import styles from './EvaluateList.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSquare } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
 type EvalType = {
+  id: number;
   name: string;
   professor: string;
 };
 
-export default function EvaluateList({ name, professor }: EvalType) {
+export default function EvaluateList({ id, name, professor }: EvalType) {
+  const navigate = useNavigate();
+
+  const goToDetailPage = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    navigate(`/evaluation/${id}`);
+  };
+
   return (
     <li className={styles.wrapper}>
       <FontAwesomeIcon icon={faSquare} color={'#97BDF5'} />
       <div className={styles.subject}>{name}</div>
       <div className={styles.professor}>{professor}</div>
-      <button className={styles.evaluation}>강의 평가</button>
+      <div className={styles.evaluation}>
+        <button onClick={goToDetailPage}>강의 평가</button>
+      </div>
     </li>
   );
 }
--- toyproject/src/components/evaluation/pages/EvaluationPage.tsx
+++ toyproject/src/components/evaluation/pages/EvaluationPage.tsx
@@ -15,25 +15,22 @@ export default function EvaluationPage() {
         <div className={styles.list}>
           <div className={styles['title-container']}>
             <div className={styles.subject}>교과목명</div>
-            <div className={styles.professor}>담당교수명</div>
+            <div className={styles.professor}>담당교수</div>
             <div className={styles.evaluation}>강의평가</div>
           </div>
           <ul>
-            <EvaluateList name={'수학장인'} professor={'대머리'}></EvaluateList>
-            <EvaluateList name={'과학장인'} professor={'대머리'}></EvaluateList>
-            <EvaluateList name={'국어장인'} professor={'대머리'}></EvaluateList>
-            <EvaluateList name={'사회장인'} professor={'대머리'}></EvaluateList>
+            {mySubjects &&
+              mySubjects.map((subject) => {
+                return (
+                  <EvaluateList
+                    key={subject.id}
+                    id={subject.id}
+                    name={subject.name}
+                    professor={subject.created_by.username}
+                  ></EvaluateList>
+                );
+              })}
           </ul>
-          {mySubjects &&
-            mySubjects.map((subject) => {
-              return (
-                <EvaluateList
-                  key={subject.id}
-                  name={subject.name}
-                  professor={subject.created_by.username}
-                ></EvaluateList>
-              );
-            })}
         </div>
       </div>
     </div>
