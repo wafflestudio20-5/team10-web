a74fb85f1fe5a2f91dfbc14989702d2ea8e3031a
panghwi <kanean99@naver.com>
2023-01-25 11:34:49 +0900

    fix / subjectlist enroll시 수강중으로 바로 변경되도록 변경

--- toyproject/src/components/selectSubject/SubjectList.tsx
+++ toyproject/src/components/selectSubject/SubjectList.tsx
@@ -1,38 +1,51 @@
-import React from 'react';
-import styles from "./SubjectList.module.scss"
-import {useSessionContext} from "../../context/SessionContext";
-import {apiEnrollClass} from "../../lib/api";
-import {toast} from "react-toastify";
+import React, { useEffect, useState } from 'react';
+import styles from './SubjectList.module.scss';
+import { useSessionContext } from '../../context/SessionContext';
+import { apiEnrollClass } from '../../lib/api';
+import { toast } from 'react-toastify';
 
 type SubjectListType = {
-    classId: number,
-    name: string,
-    professor: string
-    isEnrolled: boolean | undefined
+  classId: number;
+  name: string;
+  professor: string;
+  isEnrolled: boolean | undefined;
 };
 
-export default function SubjectList({classId, name, professor, isEnrolled}: SubjectListType) {
-    const {token, user, setUser} = useSessionContext();
-    const enroll = (token: string | null, classId: number) => {
-        apiEnrollClass(token, classId)
-            .then((r) => {
-                toast("신청되었습니다!");
-                // setUser({...user, classes: r.data.classes})
-            })
-            .catch((r) => console.log(r));
-    }
+export default function SubjectList({
+  classId,
+  name,
+  professor,
+  isEnrolled,
+}: SubjectListType) {
+  const { token, user, setUser } = useSessionContext();
+  const [subjectEnrolled, setSubjectEnrolled] = useState(isEnrolled);
+  const enroll = (token: string | null, classId: number) => {
+    apiEnrollClass(token, classId)
+      .then((r) => {
+        toast('신청되었습니다!');
+        // setUser({...user, classes: r.data.classes})
+      })
+      .then((r) => setSubjectEnrolled((prev) => !prev))
+      .catch((r) => console.log(r));
+  };
 
-    return (
-        <div className={styles.wrapper}>
-            <div className={styles.name}>{name}</div>
-            <div className={styles.professor}>{professor}</div>
-            <div className={styles.enroll}>
-                {
-                    isEnrolled ? <p>수강중</p> : <button onClick={() => {
-                        enroll(token, classId);
-                    }}>수강신청</button>
-                }
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.name}>{name}</div>
+      <div className={styles.professor}>{professor}</div>
+      <div className={styles.enroll}>
+        {subjectEnrolled ? (
+          <p>수강중</p>
+        ) : (
+          <button
+            onClick={() => {
+              enroll(token, classId);
+            }}
+          >
+            수강신청
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
