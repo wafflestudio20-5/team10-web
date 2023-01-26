c91364ce6face47dbd28bb22b540aed7fdf716ed
Chaenniee <susan2034@snu.ac.kr>
2023-01-27 02:58:46 +0900

    fix / 댓글 컴포넌트 분리

--- toyproject/src/components/board/boardDetail/BoardDetail.tsx
+++ toyproject/src/components/board/boardDetail/BoardDetail.tsx
@@ -1,14 +1,9 @@
 import React, { useState, useEffect } from "react";
 import styles from "./BoardDetail.module.scss";
+import Comment from "./Comment";
 import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
 import { PostDetail } from "../../../lib/types";
-import {
-  apiGetPost,
-  apiPostReply,
-  apiPatchReply,
-  apiDeleteReply,
-  apiDeletePost,
-} from "../../../lib/api";
+import { apiGetPost, apiDeletePost } from "../../../lib/api";
 import { timestampToDateWithDash } from "../../../lib/formatting";
 import { useSessionContext } from "../../../context/SessionContext";
 import { boardIdentifier } from "../../../lib/formatting";
@@ -24,7 +19,6 @@ export default function BoardDetail() {
   const { token } = useSessionContext();
   const { subjectid } = useParams();
   const navigate = useNavigate();
-  const [reply, setReply] = useState("");
   const [post, setPost] = useState<PostDetail>();
 
   // 게시글 세부사항 불러오기
@@ -55,50 +49,6 @@ export default function BoardDetail() {
           autoClose: 1000,
         });
         navigate(-1);
-      })
-      .catch((err) => console.log(err));
-  };
-
-  // 댓글 인풋 상자 관리
-  const handleInputReply = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    setReply(e.target.value);
-  };
-
-  // 댓글 달기
-  const postReply = (
-    token: string | null,
-    post_id: number,
-    content: string
-  ) => {
-    apiPostReply(token, post_id, content)
-      .then((res) => {
-        getPost(token, postId, category);
-        setReply("");
-      })
-      .catch((err) => console.log(err));
-  };
-
-  // 댓글 수정
-  const editReply = (
-    token: string | null,
-    comment_id: number,
-    content: string
-  ) => {
-    apiPatchReply(token, comment_id, content)
-      .then((res) => {
-        getPost(token, postId, category);
-        setReply("");
-      })
-      .catch((err) => console.log(err));
-  };
-
-  // 댓글 삭제
-  const deleteReply = (token: string | null, comment_id: number) => {
-    apiDeleteReply(token, comment_id)
-      .then((res) => {
-        getPost(token, postId, category);
-        setReply("");
       })
       .catch((err) => console.log(err));
   };
@@ -137,7 +87,7 @@ export default function BoardDetail() {
           <button
             onClick={(e) => {
               e.preventDefault();
-              // editReply(token, comment.id, reply);
+              // editPost(token, comment.id, reply);
             }}
           >
             수정
@@ -156,71 +106,12 @@ export default function BoardDetail() {
           <div className={styles.previous}>어쩌구 저쩌구</div>
         </div>
       </section>
-      <footer>
-        <h3>
-          <span>댓글</span>
-          <button className={styles.numberOfComment}>
-            {post?.comment?.length}
-          </button>
-        </h3>
-        {post?.comment?.map((comment) => {
-          return (
-            <section key={comment.id}>
-              <div className={styles.commentCreaterInfo}>
-                <span>
-                  {`${comment.created_by.username}(${comment.created_by.student_id})`}
-                </span>
-                <div className={styles.content}>
-                  {timestampToDateWithDash(Number(comment?.created_at), "date")}
-                  {` `}
-                  <FontAwesomeIcon
-                    icon={faClock}
-                    className={styles.clockIcon}
-                  />
-                  {` `}
-                  {timestampToDateWithDash(Number(comment?.created_at), "time")}
-                </div>
-                <div className={styles.commentButtons}>
-                  <button
-                    onClick={(e) => {
-                      e.preventDefault();
-                      editReply(token, comment.id, reply);
-                    }}
-                  >
-                    수정
-                  </button>
-                  <button
-                    onClick={(e) => {
-                      e.preventDefault();
-                      deleteReply(token, comment.id);
-                    }}
-                  >
-                    삭제
-                  </button>
-                </div>
-              </div>
-              <p>{comment.content}</p>
-            </section>
-          );
-        })}
-        <form>
-          <input
-            placeholder={"댓글 입력"}
-            value={reply}
-            onChange={handleInputReply}
-            className={styles.replyInput}
-          />
-          <input
-            type='submit'
-            className={styles.commentButton}
-            value='댓글 등록'
-            onClick={(e) => {
-              e.preventDefault();
-              postReply(token, postId, reply);
-            }}
-          />
-        </form>
-      </footer>
+      <Comment
+        getPost={getPost}
+        postId={postId}
+        category={category}
+        post={post}
+      />
       <ToastContainer />
     </div>
   );
