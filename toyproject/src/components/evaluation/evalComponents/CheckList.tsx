9d826699999487f03902e0ea234fe9e43baf74b5
2019dahn <>
2023-01-30 00:18:06 +0900

    feat / 강의평가 체크박스 로직 및 답변 state 구현, 제출 버튼 구현 (관련 api가 아직 없음)

--- toyproject/src/components/evaluation/evalComponents/CheckList.tsx
+++ toyproject/src/components/evaluation/evalComponents/CheckList.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import styles from './CheckList.module.scss';
 
-export default function CheckList() {
+export default function CheckList({scales, handleScales}: {
+    scales: number[],
+    handleScales: (index: number, scale: number) => void,
+}) {
   return (
     <div className={styles['grid-container']}>
       <div className={styles.title1}>평가문항</div>
@@ -10,30 +13,44 @@ export default function CheckList() {
       <EvaluateArticleAndCheckList
         index={1}
         content={'이 강의는 전체적으로 만족스러웠다.'}
+        scales={scales}
+        handleScales={handleScales}
       />
       <EvaluateArticleAndCheckList
         index={2}
         content={'강의 준비와 강의 내용이 충실하였다.'}
+        scales={scales}
+        handleScales={handleScales}
       />
       <EvaluateArticleAndCheckList
         index={3}
         content={'교육 방법이 효과적이었다.'}
+        scales={scales}
+        handleScales={handleScales}
       />
       <EvaluateArticleAndCheckList
         index={4}
         content={'이 강의는 매우 만족스러우므로 주위에 수강을 권고하겠다.'}
+        scales={scales}
+        handleScales={handleScales}
       />
       <EvaluateArticleAndCheckList
         index={5}
         content={'과제나 시험에 대한 담당 강의자의 피드백은 도움이 되었다.'}
+        scales={scales}
+        handleScales={handleScales}
       />
       <EvaluateArticleAndCheckList
         index={6}
         content={'강의자는 결강 없이 충실히 진행되었다.'}
+        scales={scales}
+        handleScales={handleScales}
       />
       <EvaluateArticleAndCheckList
         index={7}
         content={'이 강의를 통해 내 역량이 향상되었다.'}
+        scales={scales}
+        handleScales={handleScales}
       />
     </div>
   );
@@ -51,31 +68,51 @@ const EvaluateList = () => {
   );
 };
 
-const EvaluateArticleAndCheckList = ({
-  index,
-  content,
-}: {
-  index: number;
-  content: string;
+const EvaluateArticleAndCheckList = ({index, content, scales, handleScales}: {
+    index: number;
+    content: string;
+    scales: number[],
+    handleScales: (index: number, scale: number) => void,
 }) => {
+
   return (
     <>
       <div className={styles.item}>{index}.</div>
       <div className={styles.question}>{content}</div>
       <div className={styles.item}>
-        <input type='checkbox'></input>
+        <input type='checkbox' checked={scales[index-1] === 5}
+               onChange={(e) => {
+                   scales[index-1] === 5 ? handleScales(index, 0) : handleScales(index, 5)
+               }}
+        ></input>
       </div>
       <div className={styles.item}>
-        <input type='checkbox'></input>
+        <input type='checkbox' checked={scales[index-1] === 4}
+               onChange={(e) => {
+                   scales[index-1] === 4 ? handleScales(index, 0) : handleScales(index, 4)
+               }}
+        ></input>
       </div>
       <div className={styles.item}>
-        <input type='checkbox'></input>
+          <input type='checkbox' checked={scales[index-1] === 3}
+                 onChange={(e) => {
+                     scales[index-1] === 3 ? handleScales(index, 0) : handleScales(index, 3)
+                 }}
+          ></input>
       </div>
       <div className={styles.item}>
-        <input type='checkbox'></input>
+          <input type='checkbox' checked={scales[index-1] === 2}
+                 onChange={(e) => {
+                     scales[index-1] === 2 ? handleScales(index, 0) : handleScales(index, 2)
+                 }}
+          ></input>
       </div>
       <div className={styles.item}>
-        <input type='checkbox'></input>
+          <input type='checkbox' checked={scales[index-1] === 1}
+                 onChange={(e) => {
+                     scales[index-1] === 1 ? handleScales(index, 0) : handleScales(index, 1)
+                 }}
+          ></input>
       </div>
     </>
   );
--- toyproject/src/components/evaluation/pages/EvaluationDetailPage.tsx
+++ toyproject/src/components/evaluation/pages/EvaluationDetailPage.tsx
@@ -1,13 +1,16 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './EvaluationDetailPage.module.scss';
 import { SideNavBar } from '../../sideNavbar/SideNavBar';
 import CheckList from '../evalComponents/CheckList';
 import FreeAnswer from '../evalComponents/FreeAnswer';
-import { useParams } from 'react-router-dom';
+import {useNavigate, useParams} from 'react-router-dom';
+import {toast, ToastContainer} from "react-toastify";
 export default function EvaluationDetailPage() {
   const [goodPoint, setGoodPoint] = useState('');
   const [badPoint, setBadPoint] = useState('');
+  const [scales, setScales] = useState(new Array(7).fill(0));
   const { subjectid } = useParams();
+  const nav = useNavigate();
 
   const handleGoodPoint = (input: string) => {
     setGoodPoint(input);
@@ -17,6 +20,21 @@ export default function EvaluationDetailPage() {
     setBadPoint(input);
   };
 
+  const handleScales = (index: number, scale: number) => {
+    let temp = [...scales];
+    temp[index-1] = scale;
+    console.log(index, scale);
+    setScales(temp);
+    console.log("scales", scales);
+  }
+
+  const submit = () => {
+    // 아직 강의평가 제출 api가 없음
+    // scales, goodPoint, badPoint 이용해서 제출
+    toast("제출되었습니다!", {position: "top-center", theme: "colored"});
+    nav("/");
+  }
+
   return (
     <div className={styles.wrapper}>
       <SideNavBar></SideNavBar>
@@ -25,16 +43,19 @@ export default function EvaluationDetailPage() {
         <div className={styles['sub-title']}>{`${subjectid}`} 강의평가</div>
         <div className={styles.body}>
           <div className={styles.title}>공통 및 선택 문항</div>
-          <CheckList></CheckList>
+          <CheckList scales={scales} handleScales={handleScales}></CheckList>
           <FreeAnswer
             handleGoodPoint={handleGoodPoint}
             handleBadPoint={handleBadPoint}
           ></FreeAnswer>
           <div className={styles['button-container']}>
-            <button className={styles.submit}>확정</button>
+            <button className={styles.submit} onClick={() => {
+              scales.indexOf(0) === -1 ? submit() : toast("모든 문항에 답변해주시기 바랍니다", {position: "top-center", theme: "colored"});
+            }}>확정</button>
           </div>
         </div>
       </div>
+      <ToastContainer/>
     </div>
   );
 }
