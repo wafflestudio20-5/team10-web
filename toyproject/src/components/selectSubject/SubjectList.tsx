9746de9b68454a36833c035c44b5815634b87aac
2019dahn <>
2023-01-29 23:30:48 +0900

    feat / 강좌선택 페이지 수강중 -> 수강취소 버튼으로 변경 및 api 연결

--- toyproject/src/components/selectSubject/SubjectList.tsx
+++ toyproject/src/components/selectSubject/SubjectList.tsx
@@ -1,54 +1,66 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import styles from './SubjectList.module.scss';
-import { useSessionContext } from '../../context/SessionContext';
-import { apiEnrollClass } from '../../lib/api';
-import { toast } from 'react-toastify';
+import {useSessionContext} from '../../context/SessionContext';
+import {apiDropClass, apiEnrollClass} from '../../lib/api';
+import {toast} from 'react-toastify';
 
 type SubjectListType = {
-  classId: number;
-  name: string;
-  professor: string;
-  isEnrolled: boolean | undefined;
+    classId: number;
+    name: string;
+    professor: string;
+    isEnrolled: boolean | undefined;
 };
 
 export default function SubjectList({
-  classId,
-  name,
-  professor,
-  isEnrolled,
-}: SubjectListType) {
-  const { token, user, setUser, refreshUserInfo } = useSessionContext();
-  const [subjectEnrolled, setSubjectEnrolled] = useState(isEnrolled);
-  const enroll = (token: string | null, classId: number) => {
-    apiEnrollClass(token, classId)
-      .then((r) => {
-        toast('신청되었습니다!');
-        // setUser({...user, classes: r.data.classes})
-      })
-      .then((r) => {
-        setSubjectEnrolled((prev) => !prev);
-        refreshUserInfo(token!); //!를 삽입함으로서 token이 항상 존재한다는 걸 알릴 수 있다.
-      })
-      .catch((r) => console.log(r));
-  };
+                                        classId,
+                                        name,
+                                        professor,
+                                        isEnrolled,
+                                    }: SubjectListType) {
+    const {token, user, setUser, refreshUserInfo} = useSessionContext();
+    const [subjectEnrolled, setSubjectEnrolled] = useState(isEnrolled);
 
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.name}>{name}</div>
-      <div className={styles.professor}>{professor}</div>
-      <div className={styles.enroll}>
-        {subjectEnrolled ? (
-          <p>수강중</p>
-        ) : (
-          <button
-            onClick={() => {
-              enroll(token, classId);
-            }}
-          >
-            수강신청
-          </button>
-        )}
-      </div>
-    </div>
-  );
+    const enroll = (token: string | null, classId: number) => {
+        apiEnrollClass(token, classId)
+            .then((r) => {
+                toast('신청되었습니다!');
+                // setUser({...user, classes: r.data.classes})
+            })
+            .then((r) => {
+                setSubjectEnrolled((prev) => !prev);
+                refreshUserInfo(token!); //!를 삽입함으로서 token이 항상 존재한다는 걸 알릴 수 있다.
+            })
+            .catch((r) => console.log(r));
+    };
+
+    const drop = (token: string | null, classId: number) => {
+        apiDropClass(token, classId)
+            .then((r) => {
+                toast('드랍되었습니다!');
+                // setUser({...user, classes: r.data.classes})
+            })
+            .then((r) => {
+                setSubjectEnrolled((prev) => !prev);
+                refreshUserInfo(token!); //!를 삽입함으로서 token이 항상 존재한다는 걸 알릴 수 있다.
+            })
+            .catch((r) => console.log(r));
+    }
+
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.name}>{name}</div>
+            <div className={styles.professor}>{professor}</div>
+            <div className={styles.enroll}>
+                {subjectEnrolled ? (
+                    <button className={styles.drop} onClick={() => {
+                        drop(token, classId);
+                    }}>수강취소</button>
+                ) : (
+                    <button onClick={() => {
+                        enroll(token, classId);
+                    }}>수강신청</button>
+                )}
+            </div>
+        </div>
+    );
 }
