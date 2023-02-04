32854eefa79adf28319dddf5c9a389d43622e109
panghwi <kanean99@naver.com>
2023-02-04 11:43:21 +0900

    fix / http로 수정

--- toyproject/src/components/UserBar/UserBar.tsx
+++ toyproject/src/components/UserBar/UserBar.tsx
@@ -14,7 +14,7 @@ export const UserBar = () => {
         className={styles.logoutButton}
         onClick={(e) => {
           if (user?.is_social_login === true) {
-            window.location.href = `https://kauth.kakao.com/oauth/logout?client_id=${CLIENT_ID}&logout_redirect_uri=http://dvn7ib10xdyoj.cloudfront.net/authentication/logout/`;
+            window.location.href = `https://kauth.kakao.com/oauth/logout?client_id=${CLIENT_ID}&logout_redirect_uri=http://waffletoyproject10.s3-website.ap-northeast-2.amazonaws.com/authentication/logout/`;
           } else {
             logout(token as string);
           }
