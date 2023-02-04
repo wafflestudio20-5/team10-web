776397403f900b097573fbf8f47de3cdd9e49e04
panghwi <kanean99@naver.com>
2023-02-04 14:21:39 +0900

    fix / 강의평가

--- toyproject/src/components/evaluation/pages/EvaluationDetailPage.tsx
+++ toyproject/src/components/evaluation/pages/EvaluationDetailPage.tsx
@@ -1,19 +1,25 @@
-import React, { useEffect, useState } from "react";
-import styles from "./EvaluationDetailPage.module.scss";
-import { SideNavBar } from "../../sideNavbar/SideNavBar";
-import CheckList from "../evalComponents/CheckList";
-import FreeAnswer from "../evalComponents/FreeAnswer";
-import { useNavigate, useParams } from "react-router-dom";
-import { toast, ToastContainer } from "react-toastify";
-import { useSessionContext } from "../../../context/SessionContext";
-import { apiEvaluate, apiGetSubjectInfo } from "../../../lib/api";
+import React, { useEffect, useState } from 'react';
+import styles from './EvaluationDetailPage.module.scss';
+import { SideNavBar } from '../../sideNavbar/SideNavBar';
+import CheckList from '../evalComponents/CheckList';
+import FreeAnswer from '../evalComponents/FreeAnswer';
+import { useNavigate, useParams } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import { useSessionContext } from '../../../context/SessionContext';
+import {
+  apiEvaluate,
+  apiGetSubjectInfo,
+  apiGetUserInfo,
+} from '../../../lib/api';
+import { CardColor, SubjectType } from '../../../lib/types';
 export default function EvaluationDetailPage() {
-  const [goodPoint, setGoodPoint] = useState("");
-  const [badPoint, setBadPoint] = useState("");
+  const [goodPoint, setGoodPoint] = useState('');
+  const [badPoint, setBadPoint] = useState('');
   const [scales, setScales] = useState(new Array(7).fill(0));
-  const [subjectName, setSubjectName] = useState("");
+  const [subjectName, setSubjectName] = useState('');
   const { subjectid } = useParams();
-  const { token, getRefreshToken, user } = useSessionContext();
+  const { token, getRefreshToken, user, setUser, setColors } =
+    useSessionContext();
   const nav = useNavigate();
 
   const handleGoodPoint = (input: string) => {
@@ -39,14 +45,30 @@ export default function EvaluationDetailPage() {
       badPoint,
       user?.id
     )
-      .then((r) => {
-        toast("제출되었습니다!", { position: "top-center", theme: "colored" });
-        nav("/evaluation/");
+      .then(async (r) => {
+        toast('제출되었습니다!', { position: 'top-center', theme: 'colored' });
+        nav('/evaluation/');
+        const localRefresh = localStorage.getItem('refresh');
+        const localUserId = Number(localStorage.getItem('userId'));
+        const res = await getRefreshToken(localRefresh ? localRefresh : 'temp'); //렌더링 시 refreshToken 요청
+        // console.log(res.data.access);
+        if (res.status === 200) {
+          const resUser = await apiGetUserInfo(localUserId, res.data.access); //이 작업을 위해선 userId가 필요한데 우선 local Storage에 저장
+          setUser(resUser.data);
+          setColors(
+            resUser.data.classes.map((c: SubjectType): CardColor => {
+              return {
+                id: c.id,
+                color: '#97bdf5',
+              };
+            })
+          );
+        }
       })
       .catch((r) => {
-        toast("제출에 실패했습니다", {
-          position: "top-center",
-          theme: "colored",
+        toast('제출에 실패했습니다', {
+          position: 'top-center',
+          theme: 'colored',
         });
       });
   };
@@ -59,9 +81,9 @@ export default function EvaluationDetailPage() {
         setSubjectName(res.data.name);
       } catch {
         const id = Number(subjectid);
-        const localRefreshToken = localStorage.getItem("refresh");
+        const localRefreshToken = localStorage.getItem('refresh');
         const resToken = await getRefreshToken(
-          localRefreshToken ? localRefreshToken : "temp"
+          localRefreshToken ? localRefreshToken : 'temp'
         );
         const newToken = resToken.data.access;
         const res = await apiGetSubjectInfo(newToken, id);
@@ -75,7 +97,7 @@ export default function EvaluationDetailPage() {
       <SideNavBar></SideNavBar>
       <div className={styles.right}>
         <div className={styles.header}>강의 평가</div>
-        <div className={styles["sub-title"]}>{`${subjectName}`} 강의평가</div>
+        <div className={styles['sub-title']}>{`${subjectName}`} 강의평가</div>
         <div className={styles.body}>
           <div className={styles.title}>선택형 문항</div>
           <CheckList scales={scales} handleScales={handleScales}></CheckList>
@@ -84,7 +106,7 @@ export default function EvaluationDetailPage() {
             handleGoodPoint={handleGoodPoint}
             handleBadPoint={handleBadPoint}
           ></FreeAnswer>
-          <div className={styles["button-container"]}>
+          <div className={styles['button-container']}>
             <button
               className={styles.cancel}
               onClick={() => {
@@ -98,9 +120,9 @@ export default function EvaluationDetailPage() {
               onClick={() => {
                 scales.indexOf(0) === -1
                   ? submit()
-                  : toast("모든 문항에 답변해주시기 바랍니다", {
-                      position: "top-center",
-                      theme: "colored",
+                  : toast('모든 문항에 답변해주시기 바랍니다', {
+                      position: 'top-center',
+                      theme: 'colored',
                     });
               }}
             >
